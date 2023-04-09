13abbca992aab353be2a6742eebc8c3286e634d7
Jon Friedman <jfriedman97@gmail.com>
2023-04-09 15:14:11 +0200

    processor: Create Audio Parameters
    Add createParameterLayout function to return all required
    input functions for the plugin.
    
    Add apvts function in header file.
    
    Signed-off-by: Jon Friedman <jfriedman97@gmail.com>

--- PluginProcessor.cpp
+++ PluginProcessor.cpp
@@ -161,7 +161,8 @@ bool AudioPluginAudioProcessor::hasEditor() const
 
 juce::AudioProcessorEditor* AudioPluginAudioProcessor::createEditor()
 {
-    return new AudioPluginAudioProcessorEditor (*this);
+    // return new AudioPluginAudioProcessorEditor (*this);
+    return new juce::GenericAudioProcessorEditor (*this);
 }
 
 //==============================================================================
@@ -179,6 +180,46 @@ void AudioPluginAudioProcessor::setStateInformation (const void* data, int sizeI
     // whose contents will have been created by the getStateInformation() call.
     juce::ignoreUnused (data, sizeInBytes);
 }
+
+juce::AudioProcessorValueTreeState::ParameterLayout
+    AudioPluginAudioProcessor::createParameterLayout()
+    {
+        juce::AudioProcessorValueTreeState::ParameterLayout layout;
+
+        layout.add(std::make_unique<juce::AudioParameterFloat>("LowCut Freq","LowCut Freq",
+                                    juce::NormalisableRange<float>(20.f,20000.f,1.f,1.f),
+                                    20.f));
+
+        layout.add(std::make_unique<juce::AudioParameterFloat>("HighCut Freq","HighCut Freq",
+                                    juce::NormalisableRange<float>(20.f,20000.f,1.f,1.f),
+                                    20000.f));
+
+        layout.add(std::make_unique<juce::AudioParameterFloat>("Peak Freq","Peak Freq",
+                                    juce::NormalisableRange<float>(20.f,20000.f,1.f,1.f),
+                                    750.f));
+
+        layout.add(std::make_unique<juce::AudioParameterFloat>("Peak Gain","Peak Gain",
+                                                               juce::NormalisableRange<float>(-24.f,24.f,0.5f,1.f),
+                                                               0.0f));
+
+        layout.add(std::make_unique<juce::AudioParameterFloat>("Peak Quality","Peak Quality",
+                                                               juce::NormalisableRange<float>(0.1f,10.f,0.05f,1.f),
+                                                               1.f));
+        juce::StringArray stringArray;
+        for(int i=0; i<4; i++)
+        {
+            juce::String str;
+            str << (12 + i * 12);
+            str << " db/Oct";
+            stringArray.add(str);
+        }
+
+        layout.add(std::make_unique<juce::AudioParameterChoice>("LowCut Slope", "LowCut Slope", stringArray, 0));
+
+        layout.add(std::make_unique<juce::AudioParameterChoice>("HighCut Slope", "HighCut Slope", stringArray, 0));
+
+        return layout;
+    }
 
 //==============================================================================
 // This creates new instances of the plugin..
