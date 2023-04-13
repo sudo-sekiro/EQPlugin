7e73c72067335821432f47c08a72dca8a6346e5d
Jon Friedman <jfriedman97@gmail.com>
2023-04-13 21:50:39 +0200

    Editor: Add sliders to GUI
    Create sliders for each filter component.
    Create function to return component.
    Add sliders to GUI.
    
    Update plugin name in makefile so that it displays in GUI.
    
    Signed-off-by: Jon Friedman <jfriedman97@gmail.com>

--- PluginEditor.cpp
+++ PluginEditor.cpp
@@ -8,7 +8,11 @@ AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAud
     juce::ignoreUnused (processorRef);
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    for (auto* comp : getComps())
+    {
+        addAndMakeVisible(comp);
+    }
+    setSize (600, 400);
 }
 
 AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
@@ -23,11 +27,38 @@ void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    // g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void AudioPluginAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    auto bounds = getLocalBounds();
+    auto responseArea = bounds.removeFromTop(bounds.getHeight() * 0.33);
+    auto lowCutArea = bounds.removeFromLeft(bounds.getWidth() * 0.33);
+    auto HighCutArea = bounds.removeFromRight(bounds.getWidth() * 0.5);
+
+    lowCutFreqSlider.setBounds(lowCutArea.removeFromTop(lowCutArea.getHeight() * 0.66));
+    lowCutSlopeSlider.setBounds(lowCutArea);
+    highCutFreqSlider.setBounds(HighCutArea.removeFromTop(HighCutArea.getHeight() * 0.66));
+    highCutSlopeSlider.setBounds(HighCutArea);
+
+    peakFreqSlider.setBounds(bounds.removeFromTop(bounds.getHeight() * 0.33f));
+    peakGainSlider.setBounds(bounds.removeFromTop(bounds.getHeight() * 0.5f));
+    PeakQualitySlider.setBounds(bounds);
+}
+
+std::vector<juce::Component*> AudioPluginAudioProcessorEditor::getComps()
+{
+    return
+    {
+        &peakFreqSlider,
+        &peakGainSlider,
+        &PeakQualitySlider,
+        &lowCutFreqSlider,
+        &highCutFreqSlider,
+        &lowCutSlopeSlider,
+        &highCutSlopeSlider
+    };
 }
--- PluginEditor.h
+++ PluginEditor.h
@@ -2,6 +2,15 @@
 
 #include "PluginProcessor.h"
 
+struct CustomRotarySlider : juce::Slider
+{
+    CustomRotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+                                        juce::Slider::TextEntryBoxPosition::NoTextBox)
+    {
+
+    }
+};
+
 //==============================================================================
 class AudioPluginAudioProcessorEditor  : public juce::AudioProcessorEditor
 {
@@ -17,6 +26,16 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     AudioPluginAudioProcessor& processorRef;
+
+    CustomRotarySlider peakFreqSlider,
+                       peakGainSlider,
+                       PeakQualitySlider,
+                       lowCutFreqSlider,
+                       highCutFreqSlider,
+                       lowCutSlopeSlider,
+                       highCutSlopeSlider;
+
+    std::vector<juce::Component*> getComps();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessorEditor)
 };
