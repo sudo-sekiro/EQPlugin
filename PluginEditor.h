1a3bb0aae316e199375e9d3bda7842e7027d1aef
Jon Friedman <jfriedman97@gmail.com>
2023-04-15 23:40:38 +0200

    editor: Draw frequency response
    Declare monoChain as public.
    
    override and declare paramter changed functions so that the monochain
    in plugin editor can be updated asyncronously to match any updates in
    the processor audio change.
    
    Multiply the magnitudes of all the filters in the chain that are not
    bypassed.
    Draw the combined response curve of each filter in the chain.
    
    Signed-off-by Jon Friedman <jfriedman97@gmail.com>

--- PluginEditor.h
+++ PluginEditor.h
@@ -12,7 +12,9 @@ struct CustomRotarySlider : juce::Slider
 };
 
 //==============================================================================
-class AudioPluginAudioProcessorEditor  : public juce::AudioProcessorEditor
+class AudioPluginAudioProcessorEditor  : public juce::AudioProcessorEditor,
+                                         juce::AudioProcessorParameter::Listener,
+                                         juce::Timer
 {
 public:
     explicit AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor&);
@@ -21,6 +23,25 @@ public:
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
+
+    void parameterValueChanged (int parameterIndex, float newValue) override;
+
+        /** Indicates that a parameter change gesture has started.
+
+            E.g. if the user is dragging a slider, this would be called with gestureIsStarting
+            being true when they first press the mouse button, and it will be called again with
+            gestureIsStarting being false when they release it.
+
+            IMPORTANT NOTE: This will be called synchronously, and many audio processors will
+            call it during their audio callback. This means that not only has your handler code
+            got to be completely thread-safe, but it's also got to be VERY fast, and avoid
+            blocking. If you need to handle this event on your message thread, use this callback
+            to trigger an AsyncUpdater or ChangeBroadcaster which you can respond to later on the
+            message thread.
+        */
+        void parameterGestureChanged (int parameterIndex, bool gestureIsStarting) override {} ;
+
+        void timerCallback() override;
 
     CustomRotarySlider peakFreqSlider,
                        peakGainSlider,
@@ -41,15 +62,17 @@ public:
                lowCutSlopeSliderAttachment,
                highCutSlopeSliderAttachment;
 
+    std::vector<juce::Component*> getComps();
+
+    MonoChain monoChain;
+
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     AudioPluginAudioProcessor& processorRef;
 
+    juce::Atomic<bool> parametersChanged { false };
 
-    std::vector<juce::Component*> getComps();
-
-    MonoChain monoChain;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessorEditor)
 };
