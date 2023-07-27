b7f0a8518f234ee67e7ef90e1638393f465ddb00
Jon Friedman <jfriedman97@gmail.com>
2023-07-27 23:07:49 +0200

    processor: Add FIFO structs
    Add Fifo struct to push and pull blocks of samples from a FIFO buffer.
    Add Channel enum to map the Left and Right audio channels to indexs.
    Add SingleChannelSampleFifostruct to read and write to the stereo channel
    FIFOs.
    
    Signed-off-by: Jon Friedman <jfriedman97@gmail.com>

--- PluginProcessor.h
+++ PluginProcessor.h
@@ -2,6 +2,129 @@
 
 #include <juce_audio_processors/juce_audio_processors.h>
 #include <juce_dsp/juce_dsp.h>
+#include <array>
+
+
+//======================================================================
+// FIFO structs
+template<typename T>
+struct Fifo
+{
+    void prepare(int numChannels, int numSamples)
+    {
+        for ( auto& buffer : buffers)
+        {
+            buffer.setSize(numChannels,
+                           numSamples,
+                           false,       // Don't clear everything
+                           true,        // Include the extra space
+                           true);       // Avoid reallocating if possible
+            buffer.clear();
+        }
+    }
+    bool push(const T& t)
+    {
+        auto write = fifo.write(1);
+        if( write.blockSize1 > 0 )
+        {
+            buffers[write.startIndex1] = t;
+            return true;
+        }
+
+        return false;
+    }
+
+    bool pull(T& t)
+    {
+        auto read =fifo.read(1);
+        if( read.blockSize1 > 0)
+        {
+            t = buffers[read.startIndex1];
+            return true;
+        }
+
+        return false;
+    }
+
+    int getNumAvailableForReading() const
+    {
+        return fifo.getNumReady();
+    }
+private:
+    static constexpr int Capacity = 30;
+    std::array<T, Capacity> buffers;
+    juce::AbstractFifo fifo {Capacity};
+};
+
+enum Channel
+{
+    Right, // Effectively 0
+    Left   // Effectively 1
+};
+
+template<typename BlockType>
+struct SingleChannelSampleFifo
+{
+    SingleChannelSampleFifo(Channel ch) : channelToUse(ch)
+    {
+        prepared.set(false);
+    }
+
+    void update(const BlockType& buffer)
+    {
+        jassert(prepared.get());
+        jassert(buffer.getNumChannels() > channelToUse );
+        auto* channelPtr = buffer.getReadPointer(channelToUse);
+
+        for (int i = 0; i < buffer.getNumSamples(); ++i)
+        {
+            pushNextSampleIntoFifo(channelPtr[i]);
+        }
+    }
+
+    void prepare(int bufferSize)
+    {
+        prepared.set(false);
+        size.set(bufferSize);
+
+        bufferToFill.setSize(1,             // Channel
+                             bufferSize,    // Num samples
+                             false,         // KeepExistingContent
+                             true,          // Clear extra space
+                             true);         // Avoid reallocating
+        audioBufferFifo.prepare(1, bufferSize);
+        fifoIndex = 0;
+        prepared.set(true);
+    }
+    //==================================================================
+    int getNumCompleteBuffersAvailable() const { return audioBufferFifo.getNumAvailableForReading(); }
+    bool isPrepared() const { return prepared.get(); }
+    int getSize() const { return size.get(); }
+    //==================================================================
+    bool getAudioBuffer(BlockType &buf) { return audioBufferFifo.pull(buf); }
+private:
+    Channel channelToUse;
+    int fifoIndex = 0;
+    Fifo<BlockType> audioBufferFifo;
+    BlockType bufferToFill;
+    juce::Atomic<bool> prepared = false;
+    juce::Atomic<int> size = 0;
+
+    void pushNextSampleIntoFifo(float sample)
+    {
+        if (fifoIndex == bufferToFill.getNumSamples())
+        {
+            auto ok = audioBufferFifo.push(bufferToFill);
+
+            juce::ignoreUnused(ok);
+
+            fifoIndex = 0;
+        }
+
+        bufferToFill.setSample(0, fifoIndex, sample);
+        ++fifoIndex;
+    }
+};
 
 enum Slope
 {
