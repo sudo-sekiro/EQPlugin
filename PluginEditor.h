b95d7b0af16e878d0d75f64c25477abf5e2401b5
Jon Friedman <jfriedman97@gmail.com>
2023-07-31 20:32:27 +0200

    editor: Add Path generator for FFT component
    Add AnalyzerPathGenerator struct.
    Add generatePath method to create a path from rendered data.
    Add getNumPathsAvailable method to check for available paths.
    Add getPath method to return paths.
    
    Signed-off-by: Jon Friedman <jfriedman97@gmail.com>

--- PluginEditor.h
+++ PluginEditor.h
@@ -78,6 +78,74 @@ private:
     Fifo<BlockType> fftDataFifo;
 };
 
+template<typename PathType>
+struct AnalyzerPathGenerator
+{
+    // Converts 'renderData[]' into a juce::Path
+    void generatePath(const std::vector<float>& renderData,
+                      juce::Rectangle<float> fftBounds,
+                      int fftSize,
+                      float binWidth,
+                      float negativeInfinity)
+
+    {
+        auto top = fftBounds.getY();
+        auto bottom = fftBounds.getHeight();
+        auto width = fftBounds.getWidth();
+
+        int numBins = (int)fftSize / 2;
+
+        PathType p;
+        p.preallocateSpace(3 * (int)fftBounds.getWidth());
+
+        auto map = [bottom, top, negativeInfinity](float v)
+        {
+            return juce::jmap(v,
+                              negativeInfinity,
+                              0.f,
+                              float(bottom),
+                              top);
+        };
+
+        auto y = map(renderData[0]);
+
+        jassert( !std::isnan(y) && !std::isinf(y) );
+
+        p.startNewSubPath(0, y);
+
+        const int pathResolution = 2; // Can draw line-to's every 'PathResolution' pixels.
+
+        for( int binNum = 1; binNum < numBins; binNum += pathResolution)
+        {
+            y = map(renderData[binNum]);
+
+            jassert( !std::isnan(y) && !std::isinf(y) );
+
+            if ( !std::isnan(y) && !std::isinf(y) )
+            {
+                auto binFreq = binNum * binWidth;
+                auto normalizedBinX = juce::mapFromLog10(binFreq, 1.f, 20000.f);
+                int binX = std::floor(normalizedBinX * width);
+                p.lineTo(binX, y);
+            }
+        }
+
+        pathFifo.push(p);
+    }
+
+    int getNumPathsAvailable() const
+    {
+        return pathFifo.getNumAvailableForReading();
+    }
+
+    bool getPath(PathType& path)
+    {
+        return pathFifo.pull(path);
+    }
+private:
+    Fifo<PathType> pathFifo;
+};
+
 struct LookAndFeel : juce::LookAndFeel_V4
 {
             virtual void drawRotarySlider (juce::Graphics& g,
