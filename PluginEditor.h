cedb0dcb1db8664efd26d3ac49ee58327a4e3341
Jon Friedman <jfriedman97@gmail.com>
2023-08-14 21:05:56 +0200

    editor: Only redraw analyser button when opening app
    Override resized function for the Analyser Button struct and
    redraw randomised path within this struct.
    This will only create a new randomised path inside the icon when
    the app is reopened instead of every repaint.
    
    Signed-off-by: Jon Friedman <jfriedman97@gmail.com>

--- PluginEditor.h
+++ PluginEditor.h
@@ -263,7 +263,29 @@ struct ResponseCurveComponent : juce::Component,
 };
 
 struct PowerButton : juce::ToggleButton { };
-struct AnalyserButton : juce::ToggleButton { };
+struct AnalyserButton : juce::ToggleButton
+{
+    void resized() override
+    {
+        auto bounds = getLocalBounds();
+        auto insetRect = bounds.reduced(4);
+
+        randomPath.clear();
+
+        juce::Random r;
+
+        randomPath.startNewSubPath(insetRect.getX(),
+                                   insetRect.getY() + insetRect.getHeight() * r.nextFloat() );
+
+        for (auto x = insetRect.getX() + 1; x < insetRect.getRight(); x += 2)
+        {
+            randomPath.lineTo(x,
+                              insetRect.getY() + insetRect.getHeight() * r.nextFloat());
+        }
+
+    }
+    juce::Path randomPath;
+};
 //==============================================================================
 class AudioPluginAudioProcessorEditor : public juce::AudioProcessorEditor
 {
